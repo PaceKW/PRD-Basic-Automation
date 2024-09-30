8d2f7cfe20089783f44e2c23d73503be51997e6e
PaceKW <kevrat21@gmail.com>
2024-09-30 17:36:09 +0700

    update: pdf and image filename

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def index():
             size = FOLIO
 
         # Membuat PDF
-        pdf_filename = f"contoh_prd_{datetime.now().strftime('%Y%m%d_%H%M%S')}.pdf"
+        pdf_filename = f"{title}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.pdf"
         pdf_path = os.path.join('static/output/pdf', pdf_filename)
 
         doc = SimpleDocTemplate(pdf_path, pagesize=size)
@@ -59,7 +59,7 @@ def index():
 
         # Mengonversi PDF menjadi gambar untuk preview
         images = convert_from_path(pdf_path)
-        preview_image_path = os.path.join('static/output/image', f'preview_{pdf_filename}.png')
+        preview_image_path = os.path.join('static/output/image', f'{title}_{pdf_filename}.png')
 
         # Menyimpan gambar preview
         if images:
